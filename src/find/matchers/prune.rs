cb131ccb1a47c290c161528c120f1b229cce9b04
mcharsley <mcharsley@google.com>
2017-02-22 13:32:10 +0000

    Added prune

---
+++ src/find/matchers/prune.rs
@@ -0,0 +1,48 @@
+use super::PathInfo;
+
+use super::SideEffectRefs;
+
+/// This matcher checks the type of the file.
+pub struct PruneMatcher {
+}
+
+impl PruneMatcher {
+    pub fn new() -> PruneMatcher {
+        PruneMatcher {}
+    }
+
+    pub fn new_box() -> Box<super::Matcher> {
+        Box::new(PruneMatcher::new())
+    }
+}
+
+impl super::Matcher for PruneMatcher {
+    fn matches(&self, _: &PathInfo, side_effects: &mut SideEffectRefs) -> bool {
+        side_effects.should_skip_current_dir = true;
+        return true;
+    }
+
+    fn has_side_effects(&self) -> bool {
+        false
+    }
+}
+#[cfg(test)]
+
+mod tests {
+    use super::super::tests::get_dir_entry_for;
+    use super::PruneMatcher;
+    use super::super::Matcher;
+    use super::super::SideEffectRefs;
+
+    #[test]
+    fn file_type_matcher() {
+        let dir = get_dir_entry_for("test_data", "simple");
+
+        let mut side_effects = SideEffectRefs::new();
+        assert!(!side_effects.should_skip_current_dir);
+        let matcher = PruneMatcher::new();
+        assert!(matcher.matches(&dir, &mut side_effects));
+        assert!(side_effects.should_skip_current_dir);
+    }
+
+}
