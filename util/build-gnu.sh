28989171ab2e40c9aabd30713db3cda75b8c53ca
Sylvestre Ledru <sylvestre@debian.org>
2021-09-07 23:01:43 +0200

    add a script to run the tests with the GNU testsuite

---
+++ util/build-gnu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+if test ! -d ../findutils.gnu; then
+    echo "Could not find ../findutils.gnu"
+    echo "git clone  https://git.savannah.gnu.org/git/findutils.git findutils.gnu"
+    exit 1
+fi
+
+# build the rust implementation
+cargo build --release
+cp target/release/find ../findutils.gnu/find.rust
+
+# Clone and build upstream repo
+cd ../findutils.gnu
+./bootstrap
+./configure --quiet --disable-gcc-warnings
+make -j "$(nproc)"
+
+# overwrite the GNU version with the rust impl
+cp find.rust find/find
+
+# Run the tests
+make check-TESTS
