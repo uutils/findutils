43027d0808bcba639c2111e55404b505766fbaf2
Tavian Barnes <tavianator@tavianator.com>
2022-02-07 22:16:52 -0500

    ci: Also run the dejagnu tests from GNU findutils

---
+++ util/diff-gnu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+export LC_COLLATE=C
+
+# Extract the failing test lines from log files
+failing_tests() {
+    sed -En 's/FAIL: ([^,:]*)[,:].*/\1/p' "$@" | sort
+}
+
+comm -3 <(failing_tests $1/*.log) <(failing_tests $2/*.log) | tr '\t' ',' | while IFS=, read old new foo; do
+    if [ -n "$old" ]; then
+        echo "::warning ::Congrats! The GNU test $old is now passing!"
+    fi
+    if [ -n "$new" ]; then
+        echo "::error ::GNU test failed: $new. $new is passing on 'main'. Maybe you have to rebase?"
+    fi
+done
