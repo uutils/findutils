cead7121185b67b3313625f96034ea98cea92e1c
Tavian Barnes <tavianator@tavianator.com>
2022-02-22 14:30:56 -0500

    ci: Fix GNU testsuite comparison
    The uploaded log artifact maintains the directory structure, so just
    compare it directly to the GNU find directory.

--- util/diff-gnu.sh
+++ util/diff-gnu.sh
@@ -6,10 +6,10 @@ export LC_COLLATE=C
 
 # Extract the failing test lines from log files
 failing_tests() {
-    sed -En 's/FAIL: ([^,:]*)[,:].*/\1/p' "$@" | sort
+    sed -En 's/FAIL: ([^,:]*)[,:].*/\1/p' "$1"/{tests,{find,xargs}/testsuite}/*.log | sort
 }
 
-comm -3 <(failing_tests $1/*.log) <(failing_tests $2/*.log) | tr '\t' ',' | while IFS=, read old new foo; do
+comm -3 <(failing_tests "$1") <(failing_tests "$2") | tr '\t' ',' | while IFS=, read old new foo; do
     if [ -n "$old" ]; then
         echo "::warning ::Congrats! The GNU test $old is now passing!"
     fi
