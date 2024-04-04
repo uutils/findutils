d94a8e9093094385b3a265b689df95c076305bd6
hanbings <hanbings@hanbings.io>
2024-04-04 17:00:46 +0800

    find: Fix panic when piped into head (#351)
    * Ignore the SIGPIPE signal.
    
    * Add description of calling mute_sigpipe_panic().

--- src/find/main.rs
+++ src/find/main.rs
@@ -5,6 +5,11 @@
 // https://opensource.org/licenses/MIT.
 
 fn main() {
+    // Ignores the SIGPIPE signal.
+    // This is to solve the problem that when find is used with a pipe character,
+    // the downstream software of the standard output stream closes the pipe and triggers a panic.
+    uucore::panic::mute_sigpipe_panic();
+
     let args = std::env::args().collect::<Vec<String>>();
     let strs: Vec<&str> = args.iter().map(std::convert::AsRef::as_ref).collect();
     let deps = findutils::find::StandardDependencies::new();
