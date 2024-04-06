3037c074dd5bcaa5ee360d33d9d79c7c1bb574c1
hanbings <hanbings@hanbings.io>
2024-04-06 21:02:30 +0800

    Restore files after no permission file test.

--- src/find/mod.rs
+++ src/find/mod.rs
@@ -1005,24 +1005,29 @@ mod tests {
         use std::{path::Path, process::Command};
 
         let path = Path::new("./test_data/no_permission");
-
-        // Because the file does not have write permission,
-        // it cannot be overwritten if it has been created.
-        if !path.exists() {
-            let _result = fs::create_dir(&path);
-            // Generate files without permissions.
-            // std::fs cannot change file permissions to 000 in normal user state,
-            // so use chmod via Command to change permissions.
-            let _output = Command::new("chmod")
-                .arg("-rwx")
-                .arg("./test_data/no_permission")
-                .output()
-                .expect("cannot set file permission");
-        }
+        let _result = fs::create_dir(&path);
+        // Generate files without permissions.
+        // std::fs cannot change file permissions to 000 in normal user state,
+        // so use chmod via Command to change permissions.
+        let _output = Command::new("chmod")
+            .arg("-rwx")
+            .arg("./test_data/no_permission")
+            .output()
+            .expect("cannot set file permission");
 
         let deps = FakeDependencies::new();
         let rc = find_main(&["find", "./test_data/no_permission"], &deps);
 
         assert_eq!(rc, 1);
+
+        if path.exists() {
+            let _result = fs::create_dir(&path);
+            // Remove the unreadable and writable status of the file to avoid affecting other tests.
+            let _output = Command::new("chmod")
+                .arg("+rwx")
+                .arg("./test_data/no_permission")
+                .output()
+                .expect("cannot set file permission");
+        }
     }
 }
