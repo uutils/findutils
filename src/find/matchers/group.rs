8ba81731ce8265a380384509423016c8a7c0cb61
hanbings <hanbings@hanbings.io>
2024-05-10 20:27:34 +0800

    Add license header.

--- src/find/matchers/group.rs
+++ src/find/matchers/group.rs
@@ -1,3 +1,8 @@
+// This file is part of the uutils findutils package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
 use super::Matcher;
 
 #[cfg(unix)]
