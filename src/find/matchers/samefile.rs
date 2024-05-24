e270705990cf6ef60eb8834972d448e462d9ace9
hanbings <hanbings@hanbings.io>
2024-05-24 22:59:34 +0800

    Fix windows module use.

--- src/find/matchers/samefile.rs
+++ src/find/matchers/samefile.rs
@@ -3,7 +3,9 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-use std::{fs::Metadata, os::unix::fs::MetadataExt};
+use std::fs::Metadata;
+#[cfg(unix)]
+use std::os::unix::fs::MetadataExt;
 
 use super::Matcher;
 
