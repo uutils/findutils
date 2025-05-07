ff350c7154575bb554f73bb25834f361f9ef794b
Jeremy Smart <jeremy3141592@gmail.com>
2025-05-07 03:08:41 -0400

    handle windows better

--- src/updatedb/main.rs
+++ src/updatedb/main.rs
@@ -4,8 +4,14 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+#[cfg(not(windows))]
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
     let strs: Vec<&str> = args.iter().map(std::convert::AsRef::as_ref).collect();
     std::process::exit(findutils::updatedb::updatedb_main(strs.as_slice()));
+}
+
+#[cfg(windows)]
+fn main() {
+    println!("updatedb is unsupported on Windows");
 }
--- tests/db_tests.rs
+++ tests/db_tests.rs
@@ -4,112 +4,126 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-#[cfg(test)]
-mod tests {
-    use std::process::Command;
+mod common;
 
-    use assert_cmd::{assert::OutputAssertExt, cargo::CommandCargoExt};
+use std::process::Command;
 
-    #[test]
-    fn test_locate_no_matches() {
-        Command::cargo_bin("locate")
-            .expect("couldn't find locate binary")
-            .args(["usr", "--database=test_data_db"])
-            .assert()
-            .failure();
-    }
+use assert_cmd::{assert::OutputAssertExt, cargo::CommandCargoExt};
 
-    #[test]
-    fn test_locate_match() {
-        Command::cargo_bin("locate")
-            .expect("couldn't find locate binary")
-            .args(["test_data", "--database=test_data_db"])
-            .assert()
-            .success();
-    }
+use crate::common::test_helpers::fix_up_slashes;
 
-    #[test]
-    fn test_locate_no_matches_basename() {
-        Command::cargo_bin("locate")
-            .expect("couldn't find locate binary")
-            .args([
-                "test_data1234567890",
-                "--basename",
-                "--database=test_data_db",
-            ])
-            .assert()
-            .failure();
-    }
+#[cfg(not(windows))]
+const DB_FLAG: &str = "--database=test_data_db";
 
-    #[test]
-    fn test_locate_match_basename() {
-        Command::cargo_bin("locate")
-            .expect("couldn't find locate binary")
-            .args(["abbbc", "--basename", "--database=test_data_db"])
-            .assert()
-            .success();
-    }
+#[test]
+#[cfg(not(windows))]
+fn test_locate_no_matches() {
+    Command::cargo_bin("locate")
+        .expect("couldn't find locate binary")
+        .args(["usr", DB_FLAG])
+        .assert()
+        .failure();
+}
 
-    #[test]
-    fn test_locate_existing() {
-        Command::cargo_bin("locate")
-            .expect("couldn't find locate binary")
-            .args(["abbbc", "--existing", "--database=test_data_db"])
-            .assert()
-            .success();
-    }
+#[test]
+#[cfg(not(windows))]
+fn test_locate_match() {
+    Command::cargo_bin("locate")
+        .expect("couldn't find locate binary")
+        .args(["test_data", DB_FLAG])
+        .assert()
+        .success();
+}
 
-    #[test]
-    fn test_locate_non_existing() {
-        Command::cargo_bin("locate")
-            .expect("couldn't find locate binary")
-            .args(["abbbc", "--non-existing", "--database=test_data_db"])
-            .assert()
-            .failure();
-    }
+#[test]
+#[cfg(not(windows))]
+fn test_locate_no_matches_basename() {
+    Command::cargo_bin("locate")
+        .expect("couldn't find locate binary")
+        .args(["test_data1234567890", "--basename", DB_FLAG])
+        .assert()
+        .failure();
+}
 
-    #[test]
-    fn test_locate_statistics() {
-        Command::cargo_bin("locate")
-            .expect("couldn't find locate binary")
-            .args(["abbbc", "--statistics", "--database=test_data_db"])
-            .assert()
-            .success();
-    }
+#[test]
+#[cfg(not(windows))]
+fn test_locate_match_basename() {
+    Command::cargo_bin("locate")
+        .expect("couldn't find locate binary")
+        .args(["abbbc", "--basename", DB_FLAG])
+        .assert()
+        .success();
+}
 
-    #[test]
-    fn test_locate_regex() {
-        Command::cargo_bin("locate")
-            .expect("couldn't find locate binary")
-            .args(["abbbc", "--regex", "--database=test_data_db"])
-            .assert()
-            .success();
-    }
+#[test]
+#[cfg(not(windows))]
+fn test_locate_existing() {
+    Command::cargo_bin("locate")
+        .expect("couldn't find locate binary")
+        .args(["abbbc", "--existing", "--database={DB}"])
+        .assert()
+        .success();
+}
 
-    #[test]
-    fn test_locate_all() {
-        Command::cargo_bin("locate")
-            .expect("couldn't find locate binary")
-            .args(["abb", "bbc", "--regex", "--database=test_data_db"])
-            .assert()
-            .success();
-    }
+#[test]
+#[cfg(not(windows))]
+fn test_locate_non_existing() {
+    Command::cargo_bin("locate")
+        .expect("couldn't find locate binary")
+        .args(["abbbc", "--non-existing", DB_FLAG])
+        .assert()
+        .failure();
+}
 
-    #[test]
-    fn test_locate_all_regex() {
-        Command::cargo_bin("locate")
-            .expect("couldn't find locate binary")
-            .args(["abb", "b*c", "--regex", "--database=test_data_db"])
-            .assert()
-            .success();
-    }
+#[test]
+#[cfg(not(windows))]
+fn test_locate_statistics() {
+    Command::cargo_bin("locate")
+        .expect("couldn't find locate binary")
+        .args(["abbbc", "--statistics", DB_FLAG])
+        .assert()
+        .success();
+}
 
-    #[test]
-    fn test_updatedb() {
-        Command::cargo_bin("updatedb")
-            .expect("couldn't find updatedb binary")
-            .args(["--localpaths=./test_data", "--output=/dev/null"])
-            .assert()
-            .success();
-    }
+#[test]
+#[cfg(not(windows))]
+fn test_locate_regex() {
+    Command::cargo_bin("locate")
+        .expect("couldn't find locate binary")
+        .args(["abbbc", "--regex", DB_FLAG])
+        .assert()
+        .success();
+}
+
+#[test]
+#[cfg(not(windows))]
+fn test_locate_all() {
+    Command::cargo_bin("locate")
+        .expect("couldn't find locate binary")
+        .args(["abb", "bbc", "--regex", DB_FLAG])
+        .assert()
+        .success();
+}
+
+#[test]
+#[cfg(not(windows))]
+fn test_locate_all_regex() {
+    Command::cargo_bin("locate")
+        .expect("couldn't find locate binary")
+        .args(["abb", "b*c", "--regex", DB_FLAG])
+        .assert()
+        .success();
+}
+
+#[test]
+#[cfg(not(windows))]
+fn test_updatedb() {
+    Command::cargo_bin("updatedb")
+        .expect("couldn't find updatedb binary")
+        .args([
+            fix_up_slashes("--localpaths=./test_data"),
+            fix_up_slashes("--output=./test_data_db"),
+        ])
+        .assert()
+        .success();
 }
