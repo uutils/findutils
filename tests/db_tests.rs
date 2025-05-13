1e73b3f474ad4b951ce281d063ce78bdab740b5b
Jeremy Smart <jeremy3141592@gmail.com>
2025-05-13 19:45:11 -0400

    remove license lines

--- tests/db_tests.rs
+++ tests/db_tests.rs
@@ -1,5 +1,3 @@
-// Copyright 2017 Google Inc.
-//
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
